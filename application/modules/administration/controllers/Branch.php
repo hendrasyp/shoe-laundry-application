9cc1d5aee4df1b3e56dce6c78460e7d40a9fab6c
Suhendra Y Putra <suhendra.yohana@gmail.com>
2022-08-06 04:02:06 +0700

    asdasd

--- application/modules/administration/controllers/Branch.php
+++ application/modules/administration/controllers/Branch.php
@@ -45,6 +45,7 @@ class Branch extends MX_Controller {
         print_r($_POST);
         echo "<br/>INPUT_POST";
         print_r($this->input->post());
+        echo json_encode($this->input->post());
         die();
 
         $input = $this->input->post();
