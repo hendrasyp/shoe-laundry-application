d623a162e480f24cef7d2ecfe6a9406a89bacfad
Suhendra Y Putra <suhendra.yohana@gmail.com>
2022-08-06 12:14:29 +0700

    Karek bener wayah kieu

--- application/modules/administration/controllers/Branch.php
+++ application/modules/administration/controllers/Branch.php
@@ -36,46 +36,46 @@ class Branch extends MX_Controller {
     }
 
     public function update() {
-        echo "<pre>";
-        echo "REQUEST";
-        print_r($_REQUEST);
-        echo "<br/>GET";
-        print_r($_GET);
-        echo "<br/>POST";
-        print_r($_POST);
-        echo "<br/>INPUT_POST";
-        print_r($this->input->post());
-        echo json_encode($this->input->post());
-        die();
+//        echo "<pre>";
+//        echo "REQUEST";
+//        print_r($_REQUEST);
+//        echo "<br/>GET";
+//        print_r($_GET);
+//        echo "<br/>POST";
+//        print_r($_POST);
+//        echo "<br/>INPUT_POST";
+//        print_r($this->input->post());
+//        echo json_encode($this->input->post());
+//        die();
 
         $input = $this->input->post();
-        $json = file_get_contents('php://input');
-        $obj = json_decode($json);
-        do_debug($obj);
-        do_debug($_SERVER["CONTENT_TYPE"]);
-        die();
+//        $json = file_get_contents('php://input');
+//        $obj = json_decode($json);
+//        do_debug($obj);
+//        do_debug($_SERVER["CONTENT_TYPE"]);
+//        die();
 //        do_debug($input, true);
 //
-//        $toInsert = array(
-//          'name' => $input['txtName'],
-//          'address' => $input['txtAddress'],
-//          'phone' => $input['txtPhone'],
-//          'email' => $input['txtEmail'],
-//          'mapurl' => $input['txtGoogleMap'],
-//          'contact_person' => $input['txtContactPerson']
-//        );
-//
-//        $update = $this
-//            ->common_model
-//            ->table_name(TABLE_COUNTER_LOCATION)
-//            ->condition(array('id' => $input['txtCompanyId']))
-//            ->data_request($toInsert)
-//            ->do_update();
-//
-//        $response = array('message' => 'success');
-//        if ($update == false) {
-//            $response = array('message' => 'error', 'message_details' => 'Terdapat kesalahan saat update.');
-//        }
+        $toInsert = array(
+          'name' => $input['txtName'],
+          'address' => $input['txtAddress'],
+          'phone' => $input['txtPhone'],
+          'email' => $input['txtEmail'],
+          'mapurl' => $input['txtGoogleMap'],
+          'contact_person' => $input['txtContactPerson']
+        );
+
+        $update = $this
+            ->common_model
+            ->table_name(TABLE_COUNTER_LOCATION)
+            ->condition(array('id' => $input['txtCompanyId']))
+            ->data_request($toInsert)
+            ->do_update();
+
+        $response = array('message' => 'success');
+        if ($update == false) {
+            $response = array('message' => 'error', 'message_details' => 'Terdapat kesalahan saat update.');
+        }
         echo json_encode($input);
     }
 
